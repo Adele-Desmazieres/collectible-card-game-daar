3cc1876c3a4635ba7c63f8ec483c28da3aa555ff
Delta-Chat <adele.desmazi@gmail.com>
2024-10-15 10:57:58 +0200

    Nothings working

--- contracts/src/Card.sol
+++ contracts/src/Card.sol
@@ -1,19 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
+import "./Extension.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Card is Ownable {
 	
+	address owner;
+	Extension extension;
 	
+	constructor(address initialOwner) {
+		
+	}
 	
 }
-
-
-
-
-
-
-
-
 
